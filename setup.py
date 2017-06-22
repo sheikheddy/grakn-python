85a48975f645cc17d9861c7273dfe6d0f546f395
Felix Chapman <aelred717@gmail.com>
2017-06-22 14:30:05 +0100

    Update `setup.py` description, email and keywords

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setup(
     name='grakn',
     packages=['grakn'],
     version='0.1',
-    description='Python driver for Grakn',
+    description='A Python client for Grakn',
     long_description=open('README.rst').read(),
     author='Grakn Labs',
-    author_email='<whatshouldgoherewhoknows>@grakn.ai',
+    author_email='admin@grakn.ai',
     url='https://github.com/graknlabs/grakn-python',
     download_url='https://github.com/graknlabs/grakn-python/archive/0.1.tar.gz',
-    keywords=['<we should also add some keywords>'],
+    keywords=['grakn', 'database', 'graph'],
     classifiers=[],
     install_requires=['requests']
 )
