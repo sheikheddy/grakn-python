dff768c42a913b245232dd7c9696bc15d4c64bba
Felix Chapman <aelred717@gmail.com>
2017-06-22 14:30:50 +0100

    Release v0.2

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='grakn',
     packages=['grakn'],
-    version='0.1',
+    version='0.2',
     description='A Python client for Grakn',
     long_description=open('README.rst').read(),
     author='Grakn Labs',
     author_email='admin@grakn.ai',
     url='https://github.com/graknlabs/grakn-python',
-    download_url='https://github.com/graknlabs/grakn-python/archive/0.1.tar.gz',
+    download_url='https://github.com/graknlabs/grakn-python/archive/v0.2.tar.gz',
     keywords=['grakn', 'database', 'graph'],
     classifiers=[],
     install_requires=['requests']
