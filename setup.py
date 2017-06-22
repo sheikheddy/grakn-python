6c9badc2d40cfc5a2fe2a47caf2624c199a03d53
Felix Chapman <aelred717@gmail.com>
2017-06-22 15:45:46 +0100

    Change author email

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='A Python client for Grakn',
     long_description=open('README.rst').read(),
     author='Grakn Labs',
-    author_email='admin@grakn.ai',
+    author_email='community@grakn.ai',
     url='https://github.com/graknlabs/grakn-python',
     download_url='https://github.com/graknlabs/grakn-python/archive/v0.2.tar.gz',
     keywords=['grakn', 'database', 'graph'],
