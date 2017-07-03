63b389e526f84f7b195be153130cd3f4176a7d24
Felix Chapman <aelred717@gmail.com>
2017-07-03 12:18:12 +0100

    Use lower-case steps

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -18,47 +18,47 @@ def step_impl(context: Context, ont_or_data: str, patterns: str):
     env.insert(patterns)
 
 
-@given("A broken connection to the database")
+@given("a broken connection to the database")
 def step_impl(context: Context):
     context.graph = Graph(env.broken_connection)
 
 
-@when('The user issues `(.*)`')
+@when('the user issues `(.*)`')
 def step_impl(context: Context, query: str):
     context.execute_query(query)
 
 
-@then("The response is `(.*)`")
+@then("the response is `(.*)`")
 def step_impl(context: Context, response: str):
     assert context.response == eval(response)
 
 
-@then("Return a response")
+@then("return a response")
 def step_impl(context: Context):
     assert context.received_response
 
 
 # TODO: Re-think if these steps are really the same
-@then("Return a response with (matching|new|existing) concepts")
+@then("return a response with (matching|new|existing) concepts")
 def step_impl(context: Context, concept_kind: str):
     assert len(context.response) > 0, f"Response was empty: {context.response}"
 
 
-@then("Return an empty response")
+@then("return an empty response")
 def step_impl(context: Context):
     assert context.response == []
 
 
-@then('The type "(.*)" is in the graph')
+@then('the type "(.*)" is in the graph')
 def step_impl(context: Context, label: str):
     assert env.check_type(label)
 
 
-@then('The instance with (.*) "(.*)" is in the graph')
+@then('the instance with (.*) "(.*)" is in the graph')
 def step_impl(context: Context, resource_label: str, value: str):
     assert env.check_instance(resource_label, value)
 
 
-@then("Return an error")
+@then("return an error")
 def step_impl(context: Context):
     assert context.error is not None
